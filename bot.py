7d31e4086e336589032f7ffd171667229d0c3bc6
Appu John <161853537+appujohn007@users.noreply.github.com>
2024-08-01 21:25:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -45,6 +45,9 @@ from __init__ import (
 from config import Config
 from helpers import database
 from helpers.utils import UserSettings, get_readable_file_size, get_readable_time
+import pyrogram.utils
+
+pyrogram.utils.MIN_CHANNEL_ID = -1009999999999
 
 botStartTime = time.time()
 parent_id = Config.GDRIVE_FOLDER_ID
